aec5b14e80975c8f0dae6396f22134a3717cc0ab
AbdMamun1999 <mamunrox@gmail.com>
2022-03-24 15:50:11 +0600

    added main functionality

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -14,10 +14,37 @@ const Shop = () => {
         .then(data => setProducts(data))
     },[])
 
-    const addToHandleCart = product =>{
-        const newCart = [...cart,product]
+
+    useEffect(()=>{
+        const storedCart = getStoredCart()
+        const saveCart = []
+        for(const id in storedCart){
+            const addedProduct = products.find(product => product.id === id)
+            if(addedProduct){
+                const quantity = storedCart[id]
+                addedProduct.quantity = quantity
+                saveCart.push(addedProduct)
+
+            }
+        }
+        setCart(saveCart)
+    },[products])
+
+
+    const addToHandleCart = selectedProduct =>{
+        let newCart = []
+        const exists = cart.find(product => product.id === selectedProduct.id)
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart,selectedProduct]
+        }else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id)
+            exists.quantity = exists.quantity + 1
+            newCart = [...rest,exists]
+        }
+       
         setCart(newCart)
-        addToDb(product.id)
+        addToDb(selectedProduct.id)
     }
 
     return (
